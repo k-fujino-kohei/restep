787d72b2f22df6aa22aa393ceb26504a83ec1699
Kohei Fujino <appleblueberrycherry@gmail.com>
2022-08-15 12:26:11 +0900

    create endpoint macro

---
+++ src/endpoint.rs
@@ -0,0 +1,104 @@
+use darling::FromMeta;
+use proc_macro::TokenStream;
+use quote::quote;
+use std::collections::VecDeque;
+use syn::{AttributeArgs, ItemFn, Stmt, TypePath};
+
+macro_rules! unwrap_darling {
+    ($condition:expr) => {
+        match $condition {
+            Ok(v) => v,
+            Err(e) => return TokenStream::from(e.write_errors()).into(),
+        }
+    };
+}
+
+#[derive(Default, FromMeta)]
+struct EndpointArgs {
+    #[darling(default)]
+    params: Option<TypePath>,
+}
+
+pub fn parse_attr(args: AttributeArgs, item: ItemFn) -> proc_macro2::TokenStream {
+    let endpoint = unwrap_darling!(parse_endpoint(&args));
+    let path_params = unwrap_darling!(extract_path_params(&endpoint));
+    let path_params = path_params
+        .iter()
+        .map(|d| syn::Ident::from_string(d).unwrap());
+    let path_params2 = path_params.clone();
+    let args = unwrap_darling!(EndpointArgs::from_list(&args));
+
+    let fn_endpoint = if let Some(params) = args.params {
+        let params_ty = params.path.get_ident().unwrap();
+        quote! {
+            fn endpoint(params: &#params_ty) -> String {
+                #(
+                    let #path_params = &params.#path_params;
+                )*
+                format!(#endpoint, #(#path_params2 = #path_params2),*)
+            }
+        }
+    } else {
+        quote! {
+            fn endpoint() -> String {
+                format!(#endpoint)
+            }
+        }
+    };
+
+    let mut item = item;
+    let fn_endpoint = syn::parse::<Stmt>(fn_endpoint.into()).unwrap();
+    item.block.stmts.insert(0, fn_endpoint);
+
+    quote! {
+        #item
+    }
+}
+
+fn parse_endpoint(args: &AttributeArgs) -> darling::Result<String> {
+    let endpoint = args
+        .first()
+        .ok_or_else(|| darling::Error::missing_field("endpoint"))
+        .map(String::from_nested_meta)??;
+    Ok(endpoint)
+}
+
+fn extract_path_params(endpoint: &str) -> darling::Result<Vec<String>> {
+    let mut result = Vec::new();
+    let mut current = VecDeque::new();
+    let mut is_dyn = false;
+    for char in endpoint.chars() {
+        match char {
+            '{' => {
+                is_dyn = true;
+            }
+            '}' => {
+                is_dyn = false;
+                result.push(current.drain(0..).collect());
+            }
+            _ if is_dyn => {
+                current.push_back(char);
+            }
+            _ => {}
+        }
+    }
+    Ok(result)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_endpoint() {
+        assert_eq!(
+            extract_path_params("/static").unwrap(),
+            Vec::<String>::new()
+        );
+        assert_eq!(extract_path_params("/static/{id}").unwrap(), vec!["id"]);
+        assert_eq!(
+            extract_path_params("/static/{id}/{second}").unwrap(),
+            vec!["id", "second"]
+        );
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,85 @@
+//!
+//! Restep can create highly readable APIClient.
+//!
+//! # Usage
+//!
+//! Automatically generates the `endpoint()` function that returns the specified endpoint.
+//!
+//! ## Basic
+//! ```
+//! use restep::endpoint;
+//!
+//! #[endpoint("/customers")]
+//! fn simple() -> String {
+//!     // You can use `fn endpoint() -> String` in this function.
+//!     endpoint()
+//! }
+//! assert_eq!(simple(), "/customers");
+//! ```
+//!
+//! ## Path Parameters
+//! ```
+//! use restep::endpoint;
+//!
+//! struct PathParameters {
+//!     customer_id: i32,
+//! }
+//!
+//! #[endpoint("/customers/{customer_id}", params = "PathParameters")]
+//! fn dynamic_route() -> String {
+//!     let params = PathParameters { customer_id: 1 };
+//!     // You can use `fn endpoint(params: &PathParameters) -> String` in this function.
+//!     endpoint(&params)
+//! }
+//! assert_eq!(dynamic_route(), "/customers/1");
+//! ```
+//!
+//! # Examples
+//!
+//! ## RealWorld
+//! ```
+//! use restep::endpoint;
+//!
+//! #[derive(serde::Deserialize)]
+//! struct Customer {
+//!     id: i32,
+//!     name: String,
+//! }
+//!
+//! struct APIClient {
+//!     client: reqwest::Client,
+//! }
+//!
+//! struct PathParameters {
+//!     customer_id: i32,
+//! }
+//!
+//! impl APIClient {
+//!     #[endpoint("/customer/{customer_id}", params = "PathParameters")]
+//!     async fn get_customer(&self, params: PathParameters) -> anyhow::Result<Customer> {
+//!         let url = format!("{}{}", std::env::var("BASE_URL").unwrap(), endpoint(&params));
+//!         let customer = self.client
+//!             .get(url)
+//!             .send()
+//!             .await?
+//!             .json()
+//!             .await?;
+//!         Ok(customer)
+//!     }
+//! }
+//! ```
+
+mod endpoint;
+
+use crate::endpoint::parse_attr;
+use proc_macro::TokenStream;
+use syn::{parse_macro_input, AttributeArgs, ItemFn};
+
+#[proc_macro_attribute]
+pub fn endpoint(attr: TokenStream, item: TokenStream) -> TokenStream {
+    parse_attr(
+        parse_macro_input!(attr as AttributeArgs),
+        parse_macro_input!(item as ItemFn),
+    )
+    .into()
+}
